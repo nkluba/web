b023a93ddbec6ddb3460187efb0e78c5e0b74a61
nkluba <146091133+nkluba@users.noreply.github.com>
2023-12-13 14:45:40 +0000

    index file moved, updated for response parsing, flask logic for regions from db

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 import psycopg2
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(
-    dbname="peatus",
-    user="lubandust",
-    password="J9fF3NPzVeXW",
-    host="ep-holy-cake-07968363.eu-central-1.aws.neon.tech",
-    sslmode="require"
-)
+db_connection_string = "postgresql://lubandust:J9fF3NPzVeXW@ep-holy-cake-07968363.eu-central-1.aws.neon.tech/peatus?sslmode=require"
+
+
+def get_regions_from_database():
+    try:
+        connection = psycopg2.connect(db_connection_string)
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT DISTINCT stop_area FROM stops")
+        regions = [row[0] for row in cursor.fetchall()]
+        print("Regions:", regions)
+        connection.close()
+        return regions
+
+    except Exception as e:
+        print("Error fetching data from the database:", str(e))
+        return []
+
+
+@app.route('/')
+def index():
+    regions = get_regions_from_database()
+    return render_template('index.html', regions=regions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
